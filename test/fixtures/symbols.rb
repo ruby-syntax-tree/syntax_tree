43aed5c5fb5bf9ac6dcb757ac37169b822ac2a71
Kevin Newton <kddnewton@gmail.com>
2021-11-29 17:00:13 -0500

    Tests for special array literals

---
+++ test/fixtures/symbols.rb
@@ -0,0 +1,19 @@
+%
+%I[foo bar]
+%
+%I[foo #{bar}]
+%
+%I[fooooooooooooooooooooooooooooooooooooo barrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr]
+-
+%I[
+  fooooooooooooooooooooooooooooooooooooo
+  barrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+]
+%
+%I[
+  foo
+]
+-
+%I[foo]
+%
+%I[foo] # comment
