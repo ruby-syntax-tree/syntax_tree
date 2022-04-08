fb04c4a746e07e68148cfbe0c379c98d3f138271
Kevin Newton <kddnewton@gmail.com>
2022-04-07 20:30:22 -0400

    Method prefixes
    We shouldn't indent Command nodes if they end in Def/Defs/DefEndless nodes.

--- test/fixtures/command.rb
+++ test/fixtures/command.rb
@@ -5,3 +5,27 @@ foo barrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr, bazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
 -
 foo barrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr,
     bazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+%
+meta1 def foo
+end
+%
+meta2 meta1 def foo
+end
+%
+meta3 meta2 meta1 def foo
+end
+%
+meta1 def self.foo
+end
+%
+meta2 meta1 def self.foo
+end
+%
+meta3 meta2 meta1 def self.foo
+end
+%
+meta1 def foo = 1
+%
+meta2 meta1 def foo = 1
+%
+meta3 meta2 meta1 def foo = 1
