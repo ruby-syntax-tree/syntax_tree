6712db16c4bf7ad500ba9c653e0b876601134bd6
Kevin Newton <kddnewton@gmail.com>
2023-01-10 16:13:39 -0500

    Use ruby-syntax-fixtures

---
+++ test/ruby_syntax_fixtures_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+
+module SyntaxTree
+  class RubySyntaxFixturesTest < Minitest::Test
+    Dir[File.expand_path("ruby-syntax-fixtures/**/*.rb", __dir__)].each do |file|
+      define_method "test_ruby_syntax_fixtures_#{file}" do
+        refute_nil(SyntaxTree.parse(SyntaxTree.read(file)))
+      end
+    end
+  end
+end
