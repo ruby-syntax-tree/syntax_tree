a116e97dc81b59370e62885c6ab3875bf54dc522
Kevin Newton <kddnewton@gmail.com>
2023-01-11 10:19:15 -0500

    Fix up formatting on main

--- test/ruby_syntax_fixtures_test.rb
+++ test/ruby_syntax_fixtures_test.rb
@@ -4,7 +4,9 @@ require_relative "test_helper"
 
 module SyntaxTree
   class RubySyntaxFixturesTest < Minitest::Test
-    Dir[File.expand_path("ruby-syntax-fixtures/**/*.rb", __dir__)].each do |file|
+    Dir[
+      File.expand_path("ruby-syntax-fixtures/**/*.rb", __dir__)
+    ].each do |file|
       define_method "test_ruby_syntax_fixtures_#{file}" do
         refute_nil(SyntaxTree.parse(SyntaxTree.read(file)))
       end
